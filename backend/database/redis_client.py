8518dedb31e8d816df1f2da868fb858b44e9b762
11346080 <11346080@ntub.edu.tw>
2025-11-29 06:17:52 +0800

    Use REDIS_URL in redis_client

--- backend/database/redis_client.py
+++ backend/database/redis_client.py
@@ -4,11 +4,11 @@ Redis 連線管理
 from redis_om import get_redis_connection
 import redis.asyncio as aioredis
 from config import settings
+from redis.asyncio import Redis
 
-# ORM 專用 redis-om 連線
-redis_om_conn = get_redis_connection(
-    host=settings.REDIS_HOST, 
-    port=settings.REDIS_PORT
+redis = Redis.from_url(
+    settings.REDIS_URL,
+    decode_responses=True,
 )
 
 # 異步 Redis 連線
